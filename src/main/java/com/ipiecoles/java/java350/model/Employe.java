dc1a1fffb074da24b64054b3e5e0ccba0e93d339
aziz042 <164175602+aziz042@users.noreply.github.com>
2026-03-25 11:00:36 +0000

    Refactor getNbRtt method for clarity and structure

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -61,11 +61,20 @@ public class Employe {
     public Integer getNbRtt(LocalDate d){
         int i1 = d.isLeapYear() ? 365 : 366;
         int var = 104;
-        switch (LocalDate.of(d.getYear(),1,1).getDayOfWeek()){
-            case THURSDAY: if(d.isLeapYear()) var =  var + 1; break;
-            case FRIDAY: if(d.isLeapYear()) var =  var + 2; else var =  var + 1;
-            case SATURDAY: var = var + 1; break;
-        }
+     switch (LocalDate.of(d.getYear(),1,1).getDayOfWeek()) {
+    case THURSDAY:
+        if(d.isLeapYear()) var = var + 1;
+        break;
+    case FRIDAY:
+        if(d.isLeapYear()) var = var + 2;
+        else var = var + 1;
+        break;
+    case SATURDAY:
+        var = var + 1;
+        break;
+    default:
+        break; // important pour SonarCloud
+}
         int monInt = (int) Entreprise.joursFeries(d).stream().filter(localDate -> localDate.getDayOfWeek().getValue() <= DayOfWeek.FRIDAY.getValue()).count();
         return (int) Math.ceil((i1 - Entreprise.NB_JOURS_MAX_FORFAIT - var - Entreprise.NB_CONGES_BASE - monInt) * tempsPartiel);
     }
